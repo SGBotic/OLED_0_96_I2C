77192b8af7edbe9950f441430d72186e1e0dcebf
Lim KC <developer@sgbotic.com>
2017-05-19 11:37:36 +0800

    Init Commit

---
+++ OLED_0_96_I2C/OLED_0_96_I2C.ino
@@ -0,0 +1,79 @@
+/*
+
+  
+  
+  Universal 8bit Graphics Library, http://code.google.com/p/u8glib/
+  
+  Copyright (c) 2012, olikraus@gmail.com
+  All rights reserved.
+
+  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND 
+  CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, 
+  INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF OLED_0_9inchOLED_0_9inch
+  MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE 
+  DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR 
+  CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, 
+  SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT 
+  NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; 
+  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER 
+  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, 
+  STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
+  ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF 
+  ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. 
+
+*/
+/*wiring:
+
+Ardino Pro 3.3V/8MHz         OLED Display (I2C)
+GND                           GND
+3.3V                          VCC
+A4 or SDA                     SDA
+A5 or SCL                     SCL
+
+*/
+
+
+#include "U8glib.h"
+U8GLIB_SSD1306_128X64 u8g(U8G_I2C_OPT_NONE|U8G_I2C_OPT_DEV_0);  // I2C / TWI 
+
+
+void draw(void) {
+  // graphic commands to redraw the complete screen should be placed here  
+  u8g.setFont(u8g_font_unifont);
+  //u8g.setFont(u8g_font_osb21);
+  u8g.drawStr( 0, 22, "Hello World!");
+  u8g.drawStr( 0, 42, "I2C Addr 0x3C");
+}
+
+void setup(void) {
+  // flip screen, if required
+  // u8g.setRot180();
+  
+  // set SPI backup if required
+  //u8g.setHardwareBackup(u8g_backup_avr_spi);
+
+  // assign default color value
+  if ( u8g.getMode() == U8G_MODE_R3G3B2 ) {
+    u8g.setColorIndex(255);     // white
+  }
+  else if ( u8g.getMode() == U8G_MODE_GRAY2BIT ) {
+    u8g.setColorIndex(3);         // max intensity
+  }
+  else if ( u8g.getMode() == U8G_MODE_BW ) {
+    u8g.setColorIndex(1);         // pixel on
+  }
+  else if ( u8g.getMode() == U8G_MODE_HICOLOR ) {
+    u8g.setHiColorByRGB(255,255,255);
+  }
+}
+
+void loop(void) {
+  // picture loop
+  u8g.firstPage();  
+  do {
+    draw();
+  } while( u8g.nextPage() );
+  
+  // rebuild the picture after some delay
+  delay(50);
+}
---
+++ README.md
@@ -0,0 +1,10 @@
+# Monochrome 0.96" 128x64 OLED Graphic Display - I2C Interface
+Sample code for connecting 0.96" Monochrome OLED display to Arduino Pro 3.3V/8MHz using I2C interface
+
+Wiring:</br>
+Ardino Pro 3.3V/8MHz ------- OLED Display (I2C)</br>
+====================================</br>
+GND ----------------------------> GND</br>
+3.3V ----------------------------> VCC</br>
+A4 or SDA -----------------------> SDA</br>
+A5 or SCL -----------------------> SCL</br>
